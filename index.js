6c22fd3d3407baa8b9ccf24e5ffd7220ab5c3e2e
Ergin02-SenseiCRM <erginpolat0202@gmail.com>
2026-03-24 03:34:13 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-
-// Load CRM without SSR (uses localStorage)
-const CRM = dynamic(() => import('../components/CRM'), { ssr: false })
-
-export default function Home() {
-  return <CRM />
-}
